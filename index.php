d2a073d271f202807f0d978226dde96f5246c167
jhoni130977 <31085099+jhoni130977@users.noreply.github.com>
2020-06-10 07:34:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php ob_start();  ?>
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+
+<head>
+    <title>Presensi Online</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    
+	<link rel="icon" href="assets1/img/logo.png">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="assets1/css/roboto.min.css">
+    <link rel="stylesheet" href="assets1/css/tinymce.css">
+    <link rel="stylesheet" href="assets1/css/custom.min.css?">
+	<style>
+.button {
+  background-color: #333355; /* Green */
+  border: none;
+  color: white;
+  padding: 7px 24px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 24px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  
+}
+
+.button1:hover {
+box-shadow: 0 2px 6px 0 rgba(0,0,0,0.24),0 2px 10px 0 rgba(0,0,0,0.19);
+padding: 7px 24px;}
+
+.button2 {
+  background-color: #ee3333; /* Green */
+  border: none;
+  color: white;
+  padding: 7px 24px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 24px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  
+}
+
+.button3:hover {
+box-shadow: 0 2px 6px 0 rgba(0,0,0,0.24),0 2px 10px 0 rgba(0,0,0,0.19);
+padding: 7px 24px;}
+	</style>
+	
+</head>
+
+<body>
+
+    <div class="wrapper">
+		<div class="bg"><img src="assets1/img/bg-header.png"></div>
+		<div class="container">
+			<div class="header">
+				<div class="logo logo-center">
+					
+					<div>
+						<h2>&nbsp;</h2>
+						<font size=5 color=white><center>&nbsp;</font>
+					</div>
+				</div>
+			</div>
+			<div class="card login">
+				
+									
+						<table border=0 width=100% height=380px style='background-image: url(presensi.png);background-size: 100%;background-repeat: no-repeat;'>
+						<tr height=58%><td width=50%></td><td valign=top align=right><b>X</td></tr>
+						<tr><td><center>
+						<a href='https://zfrmz.com/Dw0OG2sUAw0r17DwjDeH'>
+						<button class="button2 button1" ><b>MASUK</button></a>
+						</td><td><center><a href='https://zfrmz.com/oivY0jQR82W9XNub7XMt'>
+						<button class="button button1"><b>PULANG</button></a>
+						</tr></table>
+					
+				
+			
+			</div>
+		</div>
+    </div>
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/jquery/jquery.form.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets1/js/custom.min.js"></script>
+</body>
+
+</html>
